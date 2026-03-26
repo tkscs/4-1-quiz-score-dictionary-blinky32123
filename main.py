25cec9b5ebe0a702746748aed7a9e011ee8b7c82
blinky32123 <jkrepner2028@kehillahstudent.org>
2026-03-25 23:54:32 -0700

    eeeeeeee

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # 1. Write a function `make_dictionary` that takes two lists and returns a
 # dictionary with the names as keys and the scores as values.
 def make_dictionary(keys_list, values_list):
-    """
+
+
+    """ 
     turn 2 lists into a dictionary
     
     Here are some tests. Make sure your code can handle these cases!
@@ -15,8 +17,16 @@ def make_dictionary(keys_list, values_list):
     {'key': 'value'}
     """
 
-    return #### YOUR CODE HERE
+    my_dict = {}
+    y=0
+    x=len(keys_list)
+    x=x-1
+    while y<=x:
+         my_dict[keys_list[y]] = values_list[y]
+         y=y+1
+    return my_dict
 
+###########make_dictionary(["cats", "dogs",[1,2]])
 print(make_dictionary(["a", "b"], [1, 2]))
 print(make_dictionary([1, 2, 3], [5, 6, 7]))
 print(make_dictionary(["a", "b", "c", "b"], ["apple", "banana", "clementine", "date"]))
@@ -31,17 +41,31 @@ scores = [10, 23, 13, 18, 12]
 score_dict = make_dictionary(names, scores)
 
 # 2. Using `score_dict`, find the score for "Nushi"
-#### YOUR CODE HERE
+print(score_dict["Nushi"])
 
 # 3. Add a score 19 for "John"
-#### YOUR CODE HERE
+score_dict["John"] = 19
+print(score_dict)
 
 # 4. Calculate the average of all the scores in `score_dict`
-#### YOUR CODE HERE
+sum = 0
+
+x=len(score_dict)
+
+total = 0
+for key, value in score_dict.items():
+    total += value
+
+avg = total / x
+
+print (avg)
 
 # 5. Update the score for "Wei" to be 13
-#### YOUR CODE HERE
+
+score_dict["Wei"] = 19
 
 # 6. Nushi has just dropped this class. Delete "Nushi" and his score from
 # `score_dict`
-#### YOUR CODE HERE
+
+del score_dict["Nushi"]
+print(score_dict)
